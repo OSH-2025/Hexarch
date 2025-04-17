d6d5bd04d2520cd5de602b56f737cfb23a9355ce
kyomoto <2028566723@qq.com>
2025-04-17 19:17:38 +0800

    src: 上传了部分源码以及使用bindgen生成的wrapper.rs,目前还有很多问题,如include中的FreeRTOSConfig.h其实并不太对,以及build.rs无法运行,涉及到交叉编译等很复杂的问题,目前不是我能解决的,我上传这些内容是方便大家获取源码,以及现在如果要尝试着重构,可以参考wrapper.rs中的内容,虽然帮助不是很大

---
+++ src/freeRTOS/srcC/wrapper.h
@@ -0,0 +1,8 @@
+#include <stddef.h>
+#include "include/FreeRTOS.h"
+#include "include/list.h"
+#include "include/queue.h"
+#include "include/semphr.h"
+#include "include/task.h"
+#include "include/timers.h"
+#include "portable/GCC/ARM_AARCH64/portmacro.h"
