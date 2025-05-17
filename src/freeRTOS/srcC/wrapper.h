2e06dfa7a3d55522af0596175c8d4189021e456d
Mr_Taki <2028566723@qq.com>
2025-05-17 20:43:50 +0800

    src: 重新选择了一个版本的freeRTOS

--- src/freeRTOS/srcC/wrapper.h
+++
@@ -1,8 +0,0 @@
-#include <stddef.h>
-#include "include/FreeRTOS.h"
-#include "include/list.h"
-#include "include/queue.h"
-#include "include/semphr.h"
-#include "include/task.h"
-#include "include/timers.h"
-#include "portable/GCC/ARM_AARCH64/portmacro.h"
