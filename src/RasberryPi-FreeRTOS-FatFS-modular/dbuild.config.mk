1149f797ea043ce75057e3d8f6d5b41dea035c56
Wang Chaoran <PB23111596@mail.ustc.edu.cn>
2025-06-28 21:32:01 +0800

    Delete src/RasberryPi-FreeRTOS-FatFS-modular directory

--- src/RasberryPi-FreeRTOS-FatFS-modular/dbuild.config.mk
+++
@@ -1,8 +0,0 @@
-
-CFLAGS += -march=armv6z -g -Wall -Wextra
-CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
-CFLAGS += -I $(BASE)FreeRTOS/Source/include/
-CFLAGS += -I $(BASE)FatFS/Source/
-CFLAGS += -I $(BASE)Demo/Drivers/
-
-TOOLCHAIN=arm-none-eabi-
