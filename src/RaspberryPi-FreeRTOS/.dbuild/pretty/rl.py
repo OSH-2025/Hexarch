9d196bf8f3d5a9d7651b9ea662acae273a6d99c9
VideoBus66 <sunmoonsky@mail.ustc.edu.cn>
2025-06-28 22:01:26 +0800

    Delete src/RaspberryPi-FreeRTOS directory

--- src/RaspberryPi-FreeRTOS/.dbuild/pretty/rl.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import prettyformat
-import sys
-
-for line in prettyformat.readline(sys.stdin):
-    print line
--- src/RaspberryPi-FreeRTOS/dbuild.config.mk
+++
@@ -1,7 +0,0 @@
-
-CFLAGS += -march=armv6z -g -Wall -Wextra
-CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
-CFLAGS += -I $(BASE)FreeRTOS/Source/include/
-CFLAGS += -I $(BASE)Demo/Drivers/
-
-TOOLCHAIN=arm-none-eabi-
